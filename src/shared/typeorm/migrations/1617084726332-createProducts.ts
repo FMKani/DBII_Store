f0ec5def7263c8495217f205c8cca6bf4ee7a273
Wanielton F <wanieltonferreira@gmail.com>
2021-03-30 03:34:57 -0300

    Criação da migration 'produtos'

---
+++ src/shared/typeorm/migrations/1617084726332-createProducts.ts
@@ -0,0 +1,49 @@
+import { networkInterfaces } from 'node:os';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createProducts1617084726332 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'produtos',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'nome',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'preco',
+                        type: 'decimal',
+                        precision: 10,
+                        scale: 2,
+                    },
+                    {
+                        name: 'quant',
+                        type: 'int',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('produtos');
+    }
+}
